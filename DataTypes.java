7e6600116d260bc3012ac8d7757e72af344457b0
Utthamr18 <uttham271@gmail.com>
2025-05-16 16:44:24 -0500

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        int num1 = 5;
+        byte by  = 127;
+        short sh = 558;
+        long l = 5854l;
+        float decimal = 7.5f;
+        double d = 5.8;
+        char c = 'k';
+        boolean f = false;
+
+    }
+}
