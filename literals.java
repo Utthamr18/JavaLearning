8cf62e46cdcc30f73af77ab7836580f903cb4527
Utthamr18 <uttham271@gmail.com>
2025-05-17 11:54:46 -0500

    Add files via upload

---
+++ literals.java
@@ -0,0 +1,13 @@
+public class literals {
+    public static void main(String[] args) {
+        //literals
+        int num1 = 0b101;
+        int num2 = 0x7E;
+        int num_3 = 1_00_00_00_000;
+        double num4 = 12e10;
+        boolean t = true;
+        char c = 'a';
+        System.out.println(t);
+
+    }
+}
