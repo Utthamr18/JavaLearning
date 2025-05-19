bb9456031b94250c87ac299fa3cdeb3f99d3cfe6
Utthamr18 <uttham271@gmail.com>
2025-05-18 22:11:05 -0500

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,9 @@
+class TypeCasting {
+    public static void main(String[] args) {
+        byte b = 127;
+        int i = b; 
+        int x = 132;
+        byte be = (byte)x;
+        System.out.println(be);
+    }
+}
