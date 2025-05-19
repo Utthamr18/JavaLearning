3869bc6e5383a95c7a9de6e1b29f768ef556b5d6
Utthamr18 <uttham271@gmail.com>
2025-05-18 22:58:07 -0500

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,26 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        //int num1 = 11;
+        //int num2 = 5;
+
+        //int result = num1 + num2 ;
+        //int result = num1 - num2 ;
+        //int result = num1 * num2 ;
+        //int result = num1 / num2 ;
+        //int result = num1 % num2 ;
+        //num1 = num1 + 2;
+        /*num1 += 2;
+        num1++;
+        num1--;
+        ++num1;
+        System.out.println(num1);*/
+
+        //Difference between post increment/decrement and pre increment/decrement
+        int num = 7;
+        int result = ++num;
+        int sum = num++;
+        System.out.println(result);
+        System.out.println(sum);
+        System.out.println(num);
+    }
+}
