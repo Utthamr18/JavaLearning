d436f2ff9c2d376bcf1ea90307ff4f016b9169f3
Utthamr18 <uttham271@gmail.com>
2025-06-07 18:47:58 -0500

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,38 @@
+public class ForLoop {
+    public static void main(String[] args) 
+    {
+        for(int i = 1; i <=7; i++){
+            if(i==1){
+            System.out.println("Monday has 24 hours" );
+            System.out.println("Today is work day and you need to work from 9am - 5pm");
+            }
+            if(i==2){
+            System.out.println("Tuesday has 24 hours" );
+            System.out.println("Today is work day and you need to work from 9am - 5pm");
+            }
+            if(i==3){
+            System.out.println("Wednesday has 24 hours" );
+            System.out.println("Today is work day and you need to work from 9am - 5pm");
+            }
+            if(i==4){
+            System.out.println("Thursday has 24 hours" );
+            System.out.println("Today is work day and you need to work from 9am - 5pm");
+            }
+            if(i==5){
+            System.out.println("Friday has 24 hours" );
+            System.out.println("Today is work day and you need to work from 9am - 5pm");
+            }
+            if(i==6){
+            System.out.println("Saturday has 24 hours" );
+            System.out.println("Today is not work day and you don't need to work from 9am - 5pm");
+            }
+            if(i==7){
+            System.out.println("Sunday has 24 hours" );
+            System.out.println("Today is not work day and you don't need to work from 9am - 5pm");
+            }
+            
+            
+        }
+        
+    }
+}
