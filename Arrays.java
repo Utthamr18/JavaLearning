79560fd5e14425feeaaada312259b01dce0710c7
Utthamr18 <uttham271@gmail.com>
2025-06-12 22:57:53 -0500

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,16 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int nums[] = {3,7,2,4};
+        nums[1]=6;
+        System.out.println(nums[1]);
+
+        int numsList[]  = new int[4];
+        numsList[0] = 4;
+        numsList[1] = 8;
+        numsList[2] = 3;
+        numsList[3] = 9;
+        System.out.println(numsList[1]);
+
+
+    }
+}
