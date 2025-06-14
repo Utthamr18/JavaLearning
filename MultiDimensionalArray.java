1a9f63317e755798b83fd39fed68af232f61e8b3
Utthamr18 <uttham271@gmail.com>
2025-06-14 18:58:17 -0500

    Update MultiDimensionalArray.java

--- MultiDimensionalArray.java
+++ MultiDimensionalArray.java
@@ -1,20 +1,26 @@
-
 public class MultiDimensionalArray {
     public static void main(String[] args) {
-        int nums[][] = new int[3][4];
+
+        int nums[][] = new int[3][]; // jagged array
+
+        nums[0] = new int[3];
+        nums[1] = new int[4];
+        nums[2] = new int[3];
+
         
 
-        for (int i = 0; i<3;i++){
-            for(int j = 0; j <4 ; j++){
+        for (int i = 0; i<nums.length;i++){
+            for(int j = 0; j <nums[i].length ; j++){
                 nums[i][j] = (int)(Math.random() * 10); 
                 System.out.print(nums[i][j] + " ");
             }
             System.out.println();
             
         }
+        System.out.println();
         for (int n[] : nums){
             for(int m : n){
-                System.out.println(m + " ");
+                System.out.print(m + " ");
             }
             System.out.println();
         }
